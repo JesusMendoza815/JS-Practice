610c145e292ecb0ec2de58af3118dc2b3f52c6e1
JM <jesuscamposmendoza815@gmail.com>
2022-05-05 23:13:29 -0500

    Resuelto el 22

--- js/ejercicios21-23.js
+++ js/ejercicios21-23.js
@@ -1,8 +1,14 @@
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 const arrayCuadrado = (n1,n2) => {
-    const numeros = [n1,n2];
+    let numeros = [n1,n2];
     numeros.forEach((element, i) => console.log(Math.pow(numeros[i],2)));
 }
 arrayCuadrado(2,4);
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+const nMayorMenor = (n1,n2,n3) => {
+    let array = [n1,n2,n3];
+    console.log(`Numero menor ${Math.min(...array)}, Numero mayor ${Math.max(...array)}`);
+}
+nMayorMenor(3,2,8)
+
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
