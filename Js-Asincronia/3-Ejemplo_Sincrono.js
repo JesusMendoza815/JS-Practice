60d1794dcb412129dd827a96c4c1e5a8aa22e091
JM <jesuscamposmendoza815@gmail.com>
2022-05-14 00:19:32 -0500

    Hola

---
+++ Js-Asincronia/3-Ejemplo_Sincrono.js
@@ -0,0 +1,19 @@
+function cuadradoCallback(n, callback) {
+    setTimeout(() => {
+        callback(n, n*n);
+    }, 0 | Math.random()*100);
+}
+
+cuadradoCallback(0, (value, result) => {
+    console.log("INICIA CALLBACK")
+    console.log(`Callback ${value}, ${result}`)
+    cuadradoCallback(0, (value, result) => {
+        console.log(`Callback ${value}, ${result}`)
+        cuadradoCallback(2, (value, result) => {
+            console.log(`Callback ${value}, ${result}`)
+            cuadradoCallback(5, (value, result) => {
+                console.log(`Callback ${value}, ${result}`)
+            })
+        })
+    })
+})
