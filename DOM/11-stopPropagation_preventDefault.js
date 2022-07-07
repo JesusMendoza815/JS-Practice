9e60007213291deaca2da8ac188f5d89b003df95
JM <jesuscamposmendoza815@gmail.com>
2022-07-07 18:35:18 -0500

    12 added

--- DOM/11-stopPropagation_preventDefault.js
+++ DOM/11-stopPropagation_preventDefault.js
@@ -1,8 +1,10 @@
 //DOM: Flujo de Eventos (Burbuja y Captura)
 const $eventosDivs = document.querySelectorAll('.eventos-flujo div');
+const $linkEventos = document.querySelector('.eventos-flujo a');
 
 function flijoEventos(e) {
     console.log(`Hola, te saluda ${this.className}, evento originado en ${e.target.className}`);
+    e.stopPropagation();//Detiene la propagacion 
 }
 
 
@@ -13,9 +15,15 @@ $eventosDivs.forEach(div => {
     //div.addEventListener("click", flijoEventos, false)
     // Fase de captura
     //div.addEventListener("click", flijoEventos, true)
-    capture:false,
-    once:true, //el addEventListener sólo se ejecutará una vez
+    capture:false, //flujo burbuja (false) - flujo capture (true)
+    once:false, //el addEventListener sólo se ejecutará una vez
     });
 });
+
+$linkEventos.addEventListener("click", (e) => {
+    alert(`Hola soy quien provocó el evento: ${e.target.tagName}`);
+    e.preventDefault();//previene el evento por defecto (en este caso era el de abrir un link)
+    e.stopPropagation();
+})
 
 console.log($eventosDivs);
---
+++ DOM/12-BOM.js
@@ -0,0 +1,22 @@
+//Browser Object Model
+
+// confirm('Confirmacion');
+// alert('Alerta');
+// prompt('Prompt guarda valores introducidos por el usuario');
+
+const $btnAbrir = document.querySelector('#abrir-ventana');
+const $btnCerrar = document.querySelector('#cerrar-ventana');
+const $btnImprimir = document.querySelector('#imprimir-ventana');
+
+let ventana;
+
+$btnAbrir.addEventListener('click', (e) => {
+    ventana = window.open('https://www.google.com/')
+    console.log(ventana);
+});
+$btnCerrar.addEventListener('click', (e) => {
+    ventana.close();
+});
+$btnImprimir.addEventListener('click', (e) => {
+
+});
